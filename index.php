297ae67df78dbcc1ed88df012c125ea1fb4ceda5
Samson Mathews <samsonmmathews@gmail.com>
2025-10-11 18:54:19 -0400

    Displayed player names

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FPL Database</title>
+</head>
+<body>
+
+    <h1>Fantasy Premier League Database</h1>
+
+    <?php
+        include 'connect.php';
+
+        $query = "SELECT * FROM fpl_players ORDER BY total_points DESC";
+        $result = mysqli_query($connect, $query);
+
+        if (mysqli_num_rows($result) > 0)
+        {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo $row['player_name'] . " - " . $row['team_name'] . "<br>";
+            }
+        }
+        else {
+            echo "No players found.";
+        }
+
+        mysqli_close($connect);
+    ?>
+    
+</body>
+</html>
